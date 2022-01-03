8d2c79a305411d74b17834ec91e4cea5d1bacc58
Andrew Cornies <acornies@gmail.com>
2022-01-03 13:09:51 -0500

    Working example of Vue frontend
    Signed-off-by: Andrew Cornies <acornies@gmail.com>

---
+++ static/index.js
@@ -0,0 +1,29 @@
+
+const demoApp = {
+  data: () => ({
+    todos: null,
+    loading: false,
+    timer: null
+  }),
+  methods: {
+    fetchTodos() {
+      this.loading = true;
+      fetch(`/todos`)
+        .then(response => response.json())
+        .then(response => {
+          this.loading = false;
+          this.todos = response.Todos
+        })
+        .catch(err => {
+          console.log(err.message || err);
+          this.loading = false
+        })
+    }
+  },
+  mounted: function () {
+    this.fetchTodos();
+    this.timer = setInterval(this.fetchTodos, 5000);
+  }
+};
+
+Vue.createApp(demoApp).mount('#name-list')
